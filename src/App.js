7894d2a23dd93a5864ad007b253f8d3d2dffa856
Mahmish Munawar <mahmishmunawar877@gmail.com>
2026-01-27 21:55:45 +0500

    app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Header/>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
+          <Route path='/' element={<Home />} />
         </Routes>
         <Footer/>
       </Router>
